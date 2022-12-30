43af872ec72d21aa023364fdf32373f6997c1932
real <75462154+wbreal3@users.noreply.github.com>
2022-12-30 11:51:32 +0800

    add note

--- src/java/com/real/testDemo/CopyFileDemo.java
+++ src/java/com/real/testDemo/CopyFileDemo.java
@@ -1,11 +1,17 @@
 package com.real.testDemo;
 
-import java.io.IOException;
-import java.io.RandomAccessFile;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
+
+import java.io.*;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 
 /**
+ * 对于file的io操作原始操作可使用i/o stream ,通过InPutStream将流中数据读到缓冲区中byte 再将缓冲区数据写入OutPutStream中可实现文件的复制操作
+ * 但是stream是单向的，channel管道是双向的，可使用channel进行更加高效的操作
+ * 对于日常的文件上传可使用org.apache.commons.io.FileUtils提供的copy实现文件拷贝
  * 大文件复制demo 目标
  * @author real
  * @Date 2022/12/29
@@ -48,8 +54,40 @@ public class CopyFileDemo {
         targetFile.close();
         fcIn.close();
         fcOut.close();
+    }
 
+    /**
+     * windows上创建文件
+     * 创建文件的前提
+     * @throws IOException 抛出io异常
+     */
+    @Test
+    public void fileDemoForWindows() throws IOException {
+        File file = new File("D:\\demo");
+        // 创建文件夹  'new File' is redundant
+        if (!file.exists() && !file.mkdirs()){
+            Assert.notNull(file,"文件创建失败");
+        } else {
+            // 存在文件目录直接创建文件
+            file = new File("D:\\demo\\demo.png");
+        }
+        File file1 = new File("D:\\demo\\111.png");
+        if (!file1.exists()){
+            Assert.notNull(file, file1.getName()+"不存在");
+        }
+        FileInputStream fileInputStream = new FileInputStream(file1);
+        FileOutputStream fileOutputStream = new FileOutputStream(file);
 
+        // 将文件copy到file中 1024=1kb !!! 设置此缓冲区会进行字节填充数据会是其倍数大小
+        byte[] bytes = new byte[1024*1024*10];
+
+//        FileUtils.copyFile(file1,file);
+        // 流会记忆读取的位置，下一次读取时会定位到最新位置
+        while (fileInputStream.read(bytes) != -1){
+            fileOutputStream.write(bytes);
+        }
+        fileInputStream.close();
+        fileOutputStream.close();
     }
 
 }
